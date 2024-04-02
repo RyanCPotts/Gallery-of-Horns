9bc850f9fcba70f0dd315b0d157bca71d16ee70b
RyanCPotts <ryancpotts@gmail.com>
2024-04-02 16:40:18 -0500

    all done

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -4,20 +4,21 @@ function Gallery(props) {
     const beastOne={
         title:'beast One',
         imageUrl:'https://files.worldwildlife.org/wwfcmsprod/images/Black_Rhino_in_Desert_WW176409/portrait_overview/8vo3llqoyc_Black_Rhino_in_Desert_WW176409.jpg',
-        description:'one horn'
+        description:'black rhino'
 
     }
 
     const beastTwo={
         title:'beast Two',
-        imageUrl:'https://files.worldwildlife.org/wwfcmsprod/images/Black_Rhino_in_Desert_WW176409/portrait_overview/8vo3llqoyc_Black_Rhino_in_Desert_WW176409.jpg',
-        description:'CHARGE'
+        imageUrl:'https://brightspotcdn.byu.edu/dims4/default/a22b70f/2147483647/strip/true/crop/8300x4800+0+0/resize/840x486!/quality/90/?url=https%3A%2F%2Fbrigham-young-brightspot.s3.amazonaws.com%2F54%2F85%2F5a67a65346e997d2219e9cae860a%2Fnarwhal-01.jpg',
+        description:'narwhal'
 
     }
     return (
         <>
             <HornedBeast
              beastOne={beastOne}
+             beastTwo={beastTwo}
              />
         </>
     )
--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -1,19 +1,16 @@
 function HornedBeast(props) {
-
-
-
     return (
         <>
-        <h1>Horned Beast Component</h1>
-        <h2>{props.beastOne.title}</h2>
-        <img src={props.beastOne.imageUrl} alt={props.beastOne.title} />
-        <p>{props.beastOne.description}</p>
-        
-        
-        
-        </>
-    )
+            <h1>Horned Beast Component</h1>
+            <h2>{props.beastOne.title}</h2>
+            <img src={props.beastOne.imageUrl} alt={props.beastOne.title} />
+            <p>{props.beastOne.description}</p>
 
+            <h2>{props.beastTwo.title}</h2>
+            <img src={props.beastTwo.imageUrl} alt={props.beastTwo.title} />
+            <p>{props.beastTwo.description}</p>
+        </>
+    );
 }
 
-export default HornedBeast
+export default HornedBeast;
